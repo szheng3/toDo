392614b973d4fecfcad761d53e27f71163b55dac
Sept.Griffith <szheng3@uci.edu>
2018-03-12 11:02:09 -0700

    added the css for button

--- src/components/Action.js
+++ src/components/Action.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Action = (props) => (
   <div>
-    <button
-      onClick={props.handlePick}
-      disabled={!props.hasOptions}
+      <button className='big-button'
+              onClick={props.handlePick}
+              disabled={!props.hasOptions}
     >
       What should I do?
       </button>
