23e18a740bd2773607dd9b8a720394e870bac707
Sept.Griffith <szheng3@uci.edu>
2018-03-12 13:53:17 -0700

    add the css

--- src/components/Options.js
+++ src/components/Options.js
@@ -2,19 +2,22 @@ import React from 'react';
 import Option from './Option';
 
 const Options = (props) => (
-  <div>
-      <button onClick={props.handleDeleteOptions} className='big-button button--link'>Remove All</button>
-    {props.options.length === 0 && <p>Please add an option to get started!</p>}
-    {
-      props.options.map((option) => (
-        <Option
-          key={option}
-          optionText={option}
-          handleDeleteOption={props.handleDeleteOption}
-        />
-      ))
-    }
-  </div>
+    <div>
+        <div className='widget--header'>
+            <p>Options</p>
+            <button onClick={props.handleDeleteOptions} className='button--link'>Remove All</button>
+        </div>
+        {props.options.length === 0 && <p>Please add an option to get started!</p>}
+        {
+            props.options.map((option) => (
+                <Option
+                    key={option}
+                    optionText={option}
+                    handleDeleteOption={props.handleDeleteOption}
+                />
+            ))
+        }
+    </div>
 );
 
 export default Options;
